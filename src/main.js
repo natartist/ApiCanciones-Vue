4df054a270292535d1e154502b2aa89eff827378
natartist <nata.araya@gmail.com>
2022-03-25 11:46:49 -0300

    nuevos cambios

--- src/main.js
+++ src/main.js
@@ -2,5 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.bundle.js"
+import VueSweetalert2 from "vue-sweetalert2"
+import "sweetalert2/dist/sweetalert2.min.css"
 
-createApp(App).mount('#app')
+
+createApp(App).use(VueSweetalert2).mount('#app')
